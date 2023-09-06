e43c222304dd9c47423ba2842476ce53f04a91d5
ElkhanTaghizade <140736203+ElkhanTaghizade@users.noreply.github.com>
2023-09-06 11:59:43 +0400

    Add files via upload

---
+++ Project111/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project111
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Hashtable hashtable = new Hashtable();
+            //Adding elements to the Hash table using key value pair
+            hashtable.Add("EId", 1001); //Here key is Eid and value is 1001
+            hashtable.Add("Name", "James"); //Here key is Name and value is James
+            hashtable.Add("Salary", 3500); //Here key is Salary and value is 3500
+            hashtable.Add("Location", "Mumbai"); //Here key is Location and value is Mumbai
+            hashtable.Add("EmailID", "a@a.com"); //Here key is EmailID and value is a@a.com
+            //Printing the keys and their values using foreach loop
+            Console.WriteLine("Printing Hashtable using Foreach Loop");
+            foreach (object obj in hashtable.Keys)
+            {
+                Console.WriteLine(obj + " : " + hashtable[obj]);
+            }
+        }
+    }
+}
---
+++ TERMINAL/TERMINAL/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TERMINAL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double balans= 88.5;
+            Console.WriteLine("Xos geldiniz!");
+            Console.WriteLine("Balansinizi oyrenmek ucun:1");
+            Console.WriteLine("Pul cekmek ucun:2");
+            Console.WriteLine("Balansi artirmaq ucun:3");
+            Console.WriteLine("Cixis etmek ucun:0");
+            int secim = Convert.ToInt32(Console.ReadLine());
+            if (secim == 1)
+            {
+                Console.WriteLine("Balansiniz:" + balans);
+
+            }
+
+            else if (secim == 2)
+            {
+                Console.WriteLine("Cekmek istediyiniz meblegi yazin:");
+                int mebleg = Convert.ToInt32(Console.ReadLine());
+                if (mebleg <= 88.5)
+                {
+                    Console.WriteLine("Balans:" + (balans - mebleg));
+                }
+                else
+                {
+                    Console.WriteLine("Balansiniz yeterli deyil");
+                }
+            }
+            else if(secim==3)
+            {
+                Console.WriteLine("Elave etmek istediyiniz meblegi yazin:");
+                int elave = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Balans:"+(balans+elave));
+
+
+            }
+            else if(secim==0)
+            {
+                Console.WriteLine("Tesekkurler...");
+
+            }
+            else
+            {
+                Console.WriteLine("Xahis olunur yalniz verilen reqemlerden istifade edin.");
+            }
+            Console.ReadLine();
+                      
+        }
+    }
+}
